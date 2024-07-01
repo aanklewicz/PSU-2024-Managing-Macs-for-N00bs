51141669a50daef1615aaf4e90da213edd107564
Adam Anklewicz <adam@anklewicz.com>
2024-07-01 09:52:56 -0400

    Add demo

---
+++ swiftdialog.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+/usr/local/bin/dialog --title "Swift Dialog" \
+	--message "macOS contains a built in screen reader call VoiceOver" \
+	--button2text "Dismiss" \
+	--button1text "Speak" \
+	--button1shellaction "say macOS contains a built in screen reader call VoiceOver" \
+	--icon "https://github.com/aanklewicz/PSU-2024-Managing-Macs-for-N00bs/blob/main/2024%20MacAdmins%20Logo-Circle-Blue.png?raw=true"
