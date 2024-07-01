c4c66de2609ee1cd69f4d78720351a00ea184693
Adam Anklewicz <adam@anklewicz.com>
2024-07-01 09:53:58 -0400

    Add the assignement

---
+++ swiftdialog.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+/usr/local/bin/dialog --title "Swift Dialog" \
+	--message "macOS contains a built in screen reader call VoiceOver" \
+	--button2text "Dismiss" \
+	--button1text "Speak" \
+	--button1shellaction "say macOS contains a built in screen reader call VoiceOver" \
+	--icon "https://github.com/aanklewicz/PSU-2024-Managing-Macs-for-N00bs/blob/main/2024%20MacAdmins%20Logo-Circle-Blue.png?raw=true"
+
+# Add a colour
+# Change Icon to SF Symbol
+# Add a checkbox
+# Be creative! Do something we didn't tell you to do
