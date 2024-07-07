2b19d1bf4289d78dc6c5f9da8c8bef47fff47948
Adam Anklewicz <adam@anklewicz.com>
2024-07-07 15:18:30 -0400

    Create End of Day Play Time 3.sh

---
+++ End of Day Play Time 3.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+# Create a configuration profile for Munki
+
+# Set ClientIdentifier to “Clarus”
+
+# Turn off “Install Apple Software Updates”
+
+# Set SoftwareRepoURL to “https://moof.com/repo"
+
+# AirDrop the profile to this computer, “Mr. Mustard”
