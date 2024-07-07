aaf983633e5fee2f35596b1fea647be722b86666
Adam Anklewicz <adam@anklewicz.com>
2024-07-07 15:16:40 -0400

    Create End of Day Playtime 2.sh

---
+++ End of Day Playtime 2.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+# Create a Package
+
+# Install an image of your choice to /usr/local/PSU/
+
+##################################### 
+## Set permissions:
+## Owner (you) can read and write
+## Group can read and write
+## Everyone else can read only
+#####################################
+
+# Run a script  which will write “install success” to a file in the same folder
+
+# AirDrop the package to this computer, “Mr. Mustard”
